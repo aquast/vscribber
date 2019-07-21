79298db816c6d477a42cebed3003fb8f8b329b15
Andres Quast <quast@hbz-nrw.de>
2019-07-21 22:36:51 +0200

    added complete workflow script

--- bin/dubber.sh
+++ bin/dubber.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-## A shell script to extract vob subtitles and transform them to srt subtitles
+## A shell script to copy and convert DVD Titles into mkv
 
 VSTREAMID="11";
 SUBTID="10";
+DATE=`date "+%D-%H-%M"`;
 
 while getopts hv:s: option
 do
@@ -14,7 +15,6 @@ s) SUBTID=${OPTARG};;
 esac
 done
 
-DATE=`date "+%D/%H/%M"`;
 TMPDIR="/tmp/subber/"$DATE;
 DVDDEV="/dev/dvd";
 
@@ -23,16 +23,19 @@ ls -la $TMPDIR;
 
 ## Copy Video with all its parts to temp-Dir
 mplayer dvd://$VSTREAMID -v -dumpstream -dumpfile $TMPDIR/$VSTREAMID.vob
-mplayer $TMPDIR/$VSTREAMID.vob -v -dumpstream -dumpfile $TMPDIR/$VSTREAMID.vob
+mplayer $TMPDIR/$VSTREAMID.vob -v -aid 130 -dumpaudio -dumpfile $TMPDIR/DE.aac
+mplayer $TMPDIR/$VSTREAMID.vob -v -aid 128 -dumpaudio -dumpfile $TMPDIR/EN.aac
+mplayer $TMPDIR/$VSTREAMID.vob -v -aid 129 -dumpaudio -dumpfile $TMPDIR/FR.aac
 
-ffmpeg -hwaccel cuvid -i $TMPDIR/$VSTREAMID.vob -c:v h264_nvenc -profile:v main -level 4.2 -map 0:0 $TMPDIR/$VSTREAMID.mkv
-
-
+ffmpeg -hwaccel cuvid -i $TMPDIR/$VSTREAMID.vob -c:v h264_nvenc -profile:v main -level 4.1 -map 0:0 $TMPDIR/$VSTREAMID.avi
 
 mencoder -nocache -noslices -noconfig all -o /dev/null -nosound -of rawaudio -ovc copy -vobsubout "$TMPDIR/$SUBTID.subvob" -vobsuboutindex 0 -sid $SUBTID -dvd-device $DVDDEV dvd://$VSTREAMID;
 
 subp2tiff --normalize "$TMPDIR/$SUBTID.subvob";
 find "$TMPDIR" -name "$SUBTID.*.tif" | while read -r fname ; do tesseract $fname $fname -l deu ; done;
 subptools -s -t srt -n lf -i "$TMPDIR/$SUBTID.subvob.xml" -o "$TMPDIR/$SUBTID.subvob.srt";
-ls -la $TMPDIR;
+
+
+mkvmerge -o $TMPDIR/$VSTREAMID.mkv $TMPDIR/$VSTREAMID.avi  --sync 0:850 $TMPDIR/DE.aac $TMPDIR/EN.aac $TMPDIR/FR.aac $TMPDIR/$SUBTID.subvob.srt
+
 
